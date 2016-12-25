6d0bd2e83eaa8766aa3eaf7baf0f372c8c46f010
Robbie Selwyn <robbie@selwynsoftware.com>
2016-12-24 19:43:15 -0800

    Major encapsulation to clarify and reduce code complexity

---
+++ slack_interactor.py
@@ -0,0 +1,7 @@
+import requests
+
+def send_message(data, channel="#scouting-app", icon=":exclamation:", name="Scouting Issue Bot"):
+	payload = {"channel":channel,"username": name,"text": data, "icon_emoji": icon}
+	results = pyReq.post("https://hooks.slack.com/services/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", json.dumps(payload), headers={'content-type': 'application/json'})
+
+	return results.text
