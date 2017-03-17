7a87aaf57e6ab6c4a730af3651ef705d1d4f425c
Alex Tarng <alecadad@gmail.com>
2017-03-16 23:39:35 -0700

    more framework

--- cc.py
+++ cc.py
@@ -3,5 +3,9 @@ import tba_interactor as tba
 def calculate_goals(event, period_key, goal_key):
 	# period_key: "Auto" or "Teleop"
 	# goal_key: "Fuel-High" or "Fuel-Low"
-	
+
+def calculate_defensible_oprs(event):
+
+
+def calculate_adjusted_dprs(event):
 	
