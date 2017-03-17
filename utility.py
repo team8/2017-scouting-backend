589259ee2b8096afb52534f05771be4856985922
Alex Tarng <alecadad@gmail.com>
2017-03-17 17:18:21 +0000

    asdkfjasdlkfj

--- utility.py
+++ utility.py
@@ -20,13 +20,21 @@ def test_calc(event):
     for (key) in teams:
         fb.end_of_match(event, key)
 
+def change_event_for_pit(event1, event2):
+    teams = fb.get(str(event1), "teams")
+    for (team) in teams:
+        data = fb.get(str(event1) + "/teams/", str(team))
+        pit = fb.parse_firebase_unicode(data).get("pit")
+        fb.put(str(event2) + "/teams/" + str(team), "pit", pit)
+
 fb.authenticate(firebase_secret)
-for team in tba.get_teams("2017cave"):
-    if team != 114 and team != 1661 and team != 2489 and team != 3859 and team != 3863 and team != 3882 and team != 3965 and team != 4913 and team != 5477 and team != 5678 and team != 6764:
+change_event_for_pit("2017cave_practice", "2017cave")
+#for team in tba.get_teams("2017cave"):
+#    if team != 114 and team != 1661 and team != 2489 and team != 3859 and team != 3863 and team != 3882 and team != 3965 and team != 4913 and team != 5477 and team != 5678 and team != 6764:
 #        print team
 #        if fb.get("2017cave/teams/" + str(team), "timd") == None:
 #            print "hi"
-        fb.end_of_match("2017cave", team)
+#        fb.end_of_match("2017cave", team)
 #test_calc("2017inwla")
 #change_match("2017inwla", 4103, 10, 11)
 #switch_team_match("2017inwla", 1018, "qm", 29)
